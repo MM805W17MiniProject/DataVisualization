17b23a8eb2d1a02eb25a82c9a9be8b7171f75a4d
Eclipse <xuping@ualberta.ca>
2017-03-07 20:03:06 -0700

    Added the sample project.

---
+++ dataVisualization/polls/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import include, url
+from rest_framework import routers
+
+from . import views
+
+router = routers.DefaultRouter()
+
+app_name = 'polls'
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+]
