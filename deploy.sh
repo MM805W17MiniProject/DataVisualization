fec7f611783a738855b0f3a32641eaeaa4712642
Eclipse <xuping@ualberta.ca>
2017-03-12 16:49:39 -0600

    Fix bug.

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,5 @@ git remote add api https://git.heroku.com/mm802miniproject.git
 heroku buildpacks:set heroku/python --remote api
 git commit --allow-empty -m "Deploying project"
 git push --force api master
+cd dataVisualization
 heroku run python manage.py migrate --remote api
