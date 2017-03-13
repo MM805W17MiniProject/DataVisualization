97d7a9ccd0d9663b890c87e32df9aef545c67e27
Eclipse <xuping@ualberta.ca>
2017-03-12 20:40:11 -0600

    Lets see the limit on heroku.

--- dv/fc.py
+++ dv/fc.py
@@ -34,5 +34,4 @@ def setup():
                                            month = int(splitted[4]),times = int(splitted[5]))
                 
                 cnt += 1
-		if cnt == 3000:
-		    break
+        print cnt
